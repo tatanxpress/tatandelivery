03f0ba5bbb36ce7e7ab4e9193e565f66deee2bad
tatanxpress <tatanxpress@gmail.com>
2020-05-08 14:13:49 -0600

    https

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://tatanexpress.com'),
+    'url' => env('APP_URL', 'https://tatanexpress.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
