83d3e8951c5e47e70cc467c6b30ef20e086ad4e3
tatanxpress <tatanxpress@gmail.com>
2020-06-08 10:34:11 -0600

    nuevos link 2

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -37,7 +37,7 @@
           <br>
 
           <div class="text-center">
-          <a href="https://play.google.com/store/apps/details?id=com.tatanstudios.tatanexpressapp/"><img src="{{ asset('images/google.png') }}"> &nbsp;&nbsp; </a>
+          <a href="https://play.google.com/store/apps/details?id=com.tatanstudios.tatanexpressapp"><img src="{{ asset('images/google.png') }}"> &nbsp;&nbsp; </a>
           <a href="https://apps.apple.com/us/app/tatan-express/id1512353844"><img src="{{ asset('images/apple.png') }}"></a>
           </div>         
         </div>
