f1aea97afb2c866e8cae7c9b710974d0dccd4e57
tatanxpress <tatanxpress@gmail.com>
2020-08-11 00:26:31 -0600

    m5

--- app/Http/Controllers/Api/Soap/SoapController.php
+++ app/Http/Controllers/Api/Soap/SoapController.php
@@ -12,33 +12,6 @@ class SoapController extends BaseSoapController
     public function BienesServicios(){
         try {
 
-           // ini_set('default_socket_timeout', 600);
-
-            // web service a conectar
-            self::setWsdl('https://buypasstest.redserfinsa.com:8080/BuyPass/BuyPassService.asmx?WSDL');
-           
-            // inicializar la conexion con ese web service
-            $this->service = InstanceSoapClient::init();
- 
-            $rtl = "999999999999";
-            $info = "9999995286545848";
-            $infov = "2012";
-            $infos = "123";
-
-            $params = [
-                'Info' => $info,
-                'InfoS' => $infos,
-                'Infov' => $infov,
-                'Rtl' => $rtl
-            ];
-
-            $response = $this->service->CreateCliente($params);
-            //return view ('bienes-servicios-soap', compact('response'));
-            return [$response];
-
-           // self::setWsdl('http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl');
-
-            //$url = "http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl";
 
             $url = "https://buypasstest.redserfinsa.com:8080/BuyPass/BuyPassService.asmx?WSDL";
 
@@ -46,32 +19,16 @@ class SoapController extends BaseSoapController
             $info = "9999995286545848";
             $infov = "2012";
             $infos = "123";
-
             
 
             $headerbody = array('Info'=> $info, 'InfoS' => $infos, 'Infov' => $infov, 'Rtl' => $rtl); 
 
-            $header = new \SoapClient($url, $headerbody);       
+            $header = new \SoapHeader($ns, 'CreateCliente', $headerbody);       
             
-
-            //set the Headers of Soap Client.
-            $soap_client->__CreateCliente($header);
-
+            $soap_client->__CreateCliente($header); 
+            
             return $soap_client;
 
-            $countryCode = 'DK';
-            $vatNumber = '47458714';
-
-            $params = [
-                'countryCode' => $countryCode,
-                'vatNumber'   => $vatNumber
-            ];
-
-            $response = $client->CreateCliente($rtl, $info, $infov, $infos);
-
-            //$response = $this->service->CreateCliente($rtl, $info, $infov, $infos);
-            //return view ('bienes-servicios-soap', compact('response'));
-            return [$response];
         }
         catch(\Exception $e) {
             return $e->getMessage();
